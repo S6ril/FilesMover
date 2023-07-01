4910beb6de812666499ab439bedf05e0570d99cf
S6ril <S6ril.lir6S@gmail.com>
2023-07-01 21:55:00 +0200

    add function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 mod path_utils;
 
-use std::{fs};
-
-
 
 fn main() {
     println!("Hello, world!");
@@ -15,18 +12,8 @@ fn main() {
     // Maintenant que je selectionne le répertoire literature, il faut naviguer dedans et copier coller les fichiers
     // CF python script
 
-    let file_path: String = path + "Exported Items/files/";
-
-    // println!("{file_path}");
-
-    for entry in fs::read_dir(file_path).expect("Unable to list") {
-        let entry = entry.expect("unable to get entry").path();
-        
-        if entry.is_dir() {
-            println!("{:?}", entry);
-        }
-
-    }
+    let string_path = path + "Exported Items/files/";
+    path_utils::move_and_delete_folder(string_path);
 
 
 }
--- src/path_utils.rs
+++ src/path_utils.rs
@@ -1,4 +1,4 @@
-use std::{fs::{self, ReadDir}};
+use std::{fs::{self, ReadDir}, path::{PathBuf, Path}};
 use inquire::{Select};
 
 
@@ -52,3 +52,33 @@ pub fn select_path() -> String {
 
     path
 }
+
+
+fn move_file(entry: PathBuf){
+        if entry.is_dir() {
+            println!("{:?}", entry);
+
+            for file_folder in fs::read_dir(entry).expect("Unable to list") {
+                let file_folder = file_folder.expect("unable to get entry").path();
+                
+                if file_folder.extension().unwrap() == "pdf" {                    
+                    let new_name = file_folder.parent().unwrap().parent().unwrap().join(file_folder.file_name().unwrap());
+                    fs::rename(file_folder, new_name ).unwrap();
+
+                }
+            }
+        }
+}
+
+
+pub fn move_and_delete_folder(string_path: String){
+    
+    let file_path = Path::new(&string_path );
+
+    for entry in fs::read_dir(file_path).expect("unable to get entry") {
+        let path = entry.expect("unable to get path").path();
+        move_file(path.clone());
+        let _ = fs::remove_dir_all(path);
+        
+    }
+} 
