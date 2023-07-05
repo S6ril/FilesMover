daaca58877fbd9bcdcde109feeb7b6aaf55441d1
S6ril <S6ril.lir6S@gmail.com>
2023-07-05 11:21:25 +0200

    Add display path and open in file explorer

--- src/window_utils.rs
+++ src/window_utils.rs
@@ -7,16 +7,22 @@ use crate::path_utils;
 pub struct MyApp {
     picked_path: Option<String>,
     process_folder: Option<bool>,
+    name_folder: Option<String>,
 }
 
 impl eframe::App for MyApp {
+    
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.label("Select the folder \"files\" to order the bibliography");
 
             if ui.button("Open folder…").clicked() {
                 if let Some(path) = rfd::FileDialog::new().pick_folder() {
-                    self.picked_path = Some(path.display().to_string());
+                    let path = path.display().to_string();
+                    self.picked_path = Some(path.clone());
+                    
+                    let last_folder = path.split("\\").collect::<Vec<&str>>().last().unwrap().to_string();
+                    self.name_folder = Some(last_folder);
                 }
             }
 
@@ -25,11 +31,12 @@ impl eframe::App for MyApp {
                 ui.monospace(picked_path);
                 
                 ui.vertical_centered(|ui| {
-                    // TODO name of the folder in the button.
-                    if ui.button("Process files").clicked() {
-                        println!("Process files");
-                            self.process_folder = Some(true);
-                            path_utils::move_and_delete_folder(picked_path.to_owned());
+                    if let Some(name_folder) = &self.name_folder {
+                        if ui.button("Process files:\n".to_owned() + &(name_folder)).clicked() {
+                            println!("Process files");
+                                self.process_folder = Some(true);
+                                path_utils::move_and_delete_folder(picked_path.to_owned());
+                            }
                         }
                     });
                 }
@@ -38,11 +45,18 @@ impl eframe::App for MyApp {
                 if process_folder {
                     ui.label("\nPDFs files moved !");
                     
-                    if ui.button("Exit").clicked() {
-                        frame.close();
-                    }
+                    ui.horizontal(|ui| {
+                        if ui.button("Open in file explorer").clicked() {
+                            println!("Open folder");
+                            let _ = open::that(self.picked_path.clone().unwrap());
+                        }
+                        
+                        if ui.button("Exit").clicked() {
+                            frame.close();
+                        }
+                    });
                 }
-                }
+            }
             
         });
     }
